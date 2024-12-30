39e69c21d0b61d143634486e91bf8492205e6602
khang <tranhoangkhanhvta@gmail.com>
2024-12-30 17:50:05 +0700

    add file new

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -13,6 +13,7 @@ in {
     ./hypr/default.nix
     ./nvim.nix
     ./fish.nix
+    ./git.nix
   ];
   xdg.configFile = {
     "starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${path}starship.toml";
