08767e4d4c3f0b762ee94f792624ec8f182370db
iyqht <tranhoangkhanhvta@gmail.com>
2024-12-30 21:42:09 +0700

    modified git.nix

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,9 +1,17 @@
 {
   programs.git = {
+    enable = true;
     userEmail = "tranhoangkhanhvta@gmail.com";
     userName = "iyqht";
     aliases = {
       co = "checkout";
+    };
+    extraConfig = {
+      credential = {
+        helper = "manager";
+        credentialStore = "gpg";
+        "https://github.com".username = "iyqht";
+      };
     };
   };
 }
